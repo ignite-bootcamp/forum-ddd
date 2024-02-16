b4e9cda3df9095b73d5d0686383a17e9f96092a9
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-15 23:23:22 -0300

    feat: mapping relations

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -8,7 +8,11 @@ interface AnswerQuestionUseCaseRequest {
 
 export class AnswerQuestionUseCase {
   execute({ questionId, instructorId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content);
+    const answer = new Answer({
+      content,
+      authorId: instructorId,
+      questionId,
+    });
 
     return answer;
   }
