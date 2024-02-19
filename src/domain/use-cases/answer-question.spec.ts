2025c7a31c71b3a38b85199b3d409161a6952333
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-19 20:59:36 -0300

    feat: created answer repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,16 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
-  const answer = answerQuestion.execute({
+const fakeAnswerRepository: AnswersRepository = {
+  create: async (answer) => {
+    return;
+  },
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+  const answer = await answerQuestion.execute({
     content: "Nova questão",
     instructorId: "1",
     questionId: "1",
