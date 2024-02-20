dcaf75ed63b208a6a16e2cc5caba7f4bca2f2c51
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-20 20:05:47 -0300

    feat: adding more properties to answer and question

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
   title: string;
   content: string;
-  authorId: string;
   slug: Slug;
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
