e62d0dfc04bb6da74388c4c1f8450d8f6517d8df
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-20 20:48:08 -0300

    feat: adding getters and setters for entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,8 +11,32 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get questionId() {
+    return this.props.questionId;
+  }
+  get authorId() {
+    return this.props.authorId;
+  }
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
   get content() {
     return this.props.content;
+  }
+  get excerpt() {
+    return this.content.substring(0, 120).trim().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
   }
 
   static create(
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -14,13 +14,58 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get slug() {
+    return this.props.slug;
+  }
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+  get authorId() {
+    return this.props.authorId;
+  }
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+  get title() {
+    return this.props.title;
+  }
+  get content() {
+    return this.props.content;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trim().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
   static create(
-    props: Optional<QuestionProps, "createdAt">,
+    props: Optional<QuestionProps, "createdAt" | "slug">,
     id?: UniqueEntityID,
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id,
