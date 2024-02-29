8b2f867661554996f094f7731a1b42ccd366bd1c
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-29 19:53:54 -0300

    feat: fetch questions answers use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,8 +1,13 @@
 import { Answer } from '../../enterprise/entities/answer';
+import { PaginationParams } from './pagination-params';
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>;
   findById(id: string): Promise<Answer | null>;
   delete(answer: Answer): Promise<void>;
   save(answer: Answer): Promise<void>;
+  findManyByQuestionId(
+    questionId: string,
+    param: PaginationParams,
+  ): Promise<Answer[]>;
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,4 +1,5 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
+import { PaginationParams } from '@/domain/forum/application/repositories/pagination-params';
 import { Answer } from '@/domain/forum/enterprise/entities/answer';
 
 export class InMemoryAnswersRepository implements AnswersRepository {
@@ -25,8 +26,19 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async save(answer: Answer): Promise<void> {
     const questionIndex = this.items.findIndex(
-      item => item.id.toString() === answer.id,
+      item => item.id.toString() === answer.id.toString(),
     );
     this.items[questionIndex] = answer;
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter(item => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answers;
   }
 }
