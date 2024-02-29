acd92b9360c0991f66d6ec564c1c6f759d1b2e02
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-29 20:19:31 -0300

    refactor: using comment entity in question-comment

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,41 +1,12 @@
-import { Entity } from '@/core/entities/entity';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
+import { Comment, CommentProps } from './comment';
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID;
-  content: string;
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  get content() {
-    return this.props.content;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
-  }
-
+export class QuestionComment extends Comment<QuestionCommentProps> {
   static create(
     props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityID,
