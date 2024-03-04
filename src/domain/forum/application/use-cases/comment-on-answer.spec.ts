f87e6d67304e63b24b6604fac87a2b3584324488
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-04 20:59:39 -0300

    feat: delete comment use case

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -1,12 +1,36 @@
-import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
-import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository';
+import { makeAnswer } from 'test/factories/make-answer';
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer';
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository';
 
-export class InMemoryAnswerCommentsRepository
-  implements AnswerCommentsRepository
-{
-  public items: AnswerComment[] = [];
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUseCase;
 
-  async create(answerComment: AnswerComment) {
-    this.items.push(answerComment);
-  }
-}
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    );
+  });
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer();
+
+    await inMemoryAnswersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentário teste',
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comentário teste',
+    );
+  });
+});
