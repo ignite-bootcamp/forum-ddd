efab04ab512a76200a4a57550d124ca08fa2781b
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-07 21:28:48 -0300

    feat: created aggregate root entity

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
-import { Entity } from '@/core/entities/entity';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
 import { Slug } from './value-objects/slug';
+import { AggregateRoot } from '@/core/entities/aggregate-root';
 
 export interface QuestionProps {
   title: string;
@@ -13,7 +13,7 @@ export interface QuestionProps {
   updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get slug() {
     return this.props.slug;
   }
