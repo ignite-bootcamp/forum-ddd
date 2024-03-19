761739be26b808eedd0c8782a57395dc1b8ab3aa
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-19 20:35:06 -0300

    tests: read-notification tests

---
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,49 @@
+import { makeNotification } from 'test/factories/make-notification';
+import { ReadNotificationUseCase } from './read-notification';
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let sut: ReadNotificationUseCase;
+
+describe('Read Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository();
+    sut = new ReadNotificationUseCase(inMemoryNotificationsRepository);
+  });
+
+  it('should be able to read a notification', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityID('recipient-1'),
+    });
+
+    await inMemoryNotificationsRepository.save(notification);
+
+    const result = await sut.execute({
+      notificationId: notification.id.toString(),
+      recipientId: 'recipient-1',
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a notification from other recipient', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityID('recipient-2'),
+    });
+
+    await inMemoryNotificationsRepository.save(notification);
+
+    const result = await sut.execute({
+      notificationId: notification.id.toString(),
+      recipientId: 'recipient-1',
+    });
+
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
+  });
+});
