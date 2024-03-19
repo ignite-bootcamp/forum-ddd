e9483e9c7395f46b8923b878e0f939d74228b1b9
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-19 20:24:26 -0300

    feat: read-notification use case

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -22,6 +22,9 @@ export class Notification extends Entity<NotificationProps> {
   get recipientId() {
     return this.props.recipientId;
   }
+  read() {
+    this.props.readAt = new Date();
+  }
 
   static create(
     props: Optional<NotificationProps, 'createdAt'>,
