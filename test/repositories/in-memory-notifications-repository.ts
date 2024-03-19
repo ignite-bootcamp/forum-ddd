f9f63830f2deaaa3416dda203084009bb123b567
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-03-19 20:28:00 -0300

    fix: fixing memory notifications repository

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -9,4 +9,18 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.items.push(notification);
   }
+
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.items.find(item => item.id.toString() === id);
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    this.items.push(notification);
+  }
 }
